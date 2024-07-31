f5971112f876959bfaef991d3856a18079304dea
joramleonardo <kateleonardo05@gmail.com>
2024-07-31 15:33:28 +0800

    updated

--- resources/js/store.js
+++ resources/js/store.js
@@ -1,20 +1,3 @@
-// import Vue from 'vue';
-// import Vuex from 'vuex';
-
-// Vue.use(Vuex);
-
-// export default new Vuex.Store({
-//     state: {
-//         apiURL: 'http://127.0.0.1:8000/api',
-//         serverPath: 'http://127.0.0.1:8000'
-//     },
-//     mutations: {},
-//     actions: {}
-// });
-
-
-
-
 import Vue from 'vue';
 import Vuex from 'vuex';
 
@@ -22,9 +5,26 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        apiURL: 'http://ticketing.stii.local/api',
-        serverPath: 'http://ticketing.stii.local'
+        apiURL: 'http://127.0.0.1:8000/api',
+        serverPath: 'http://127.0.0.1:8000'
     },
     mutations: {},
     actions: {}
 });
+
+
+
+
+// import Vue from 'vue';
+// import Vuex from 'vuex';
+
+// Vue.use(Vuex);
+
+// export default new Vuex.Store({
+//     state: {
+//         apiURL: 'http://ticketing.stii.local/api',
+//         serverPath: 'http://ticketing.stii.local'
+//     },
+//     mutations: {},
+//     actions: {}
+// });
